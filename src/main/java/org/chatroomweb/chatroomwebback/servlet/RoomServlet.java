d0c930eae188f6003889c936a73ebdb5853a4692
Daniel160407 <abulashvilidani@hotmail.com>
2024-03-30 23:07:08 +0400

    Initial commit

---
+++ src/main/java/org/chatroomweb/chatroomwebback/servlet/RoomServlet.java
@@ -0,0 +1,13 @@
+package org.chatroomweb.chatroomwebback.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/RoomServlet")
+public class RoomServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
