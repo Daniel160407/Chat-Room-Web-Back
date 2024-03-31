d9e54b9fe4bf05b8580af231ceff32f66f74e223
Daniel160407 <abulashvilidani@hotmail.com>
2024-03-31 23:22:32 +0400

    Added servlet functions to add and get rooms

--- src/main/java/org/chatroomweb/servlet/RoomServlet.java
+++ src/main/java/org/chatroomweb/servlet/RoomServlet.java
@@ -1,13 +1,36 @@
 package org.chatroomweb.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.chatroomweb.dao.MySQLController;
+import org.chatroomweb.request.AddNewRoomRequest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/room")
 public class RoomServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    private final MySQLController mySQLController = new MySQLController();
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/json");
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(new ObjectMapper().writeValueAsString(mySQLController.getRooms()));
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/json");
+
+        String name = request.getParameter("name");
+        Integer maxMembers = Integer.valueOf(request.getParameter("maxMembers"));
+
+        mySQLController.addRoom(new AddNewRoomRequest(name, maxMembers));
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(new ObjectMapper().writeValueAsString(mySQLController.getRooms()));
     }
 }
