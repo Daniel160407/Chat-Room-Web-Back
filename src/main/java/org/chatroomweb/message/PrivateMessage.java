34724e4ae24964f29ce23e4bed3494c84079b20c
Daniel160407 <abulashvilidani@hotmail.com>
2024-04-15 21:32:26 +0400

    Added roomName property to message classes

--- src/main/java/org/chatroomweb/message/PrivateMessage.java
+++ src/main/java/org/chatroomweb/message/PrivateMessage.java
@@ -1,27 +1,16 @@
 package org.chatroomweb.message;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ToString
 public class PrivateMessage implements Message{
     private String sid;
+    private String roomName;
     private String sender;
     private String receiver;
     private String message;
-
-    @Override
-    public String toString() {
-        return "PrivateMessage{" +
-                "sid='" + sid + '\'' +
-                ", sender='" + sender + '\'' +
-                ", receiver='" + receiver + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
 }
--- src/main/java/org/chatroomweb/message/PublicMessage.java
+++ src/main/java/org/chatroomweb/message/PublicMessage.java
@@ -1,23 +1,14 @@
 package org.chatroomweb.message;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ToString
 public class PublicMessage implements Message{
+    private String roomName;
     private String sender;
     private String message;
-
-    @Override
-    public String toString() {
-        return "PublicMessage{" +
-                "sender='" + sender + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
 }
